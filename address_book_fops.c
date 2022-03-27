de93454b854bb3b3516ea2d79ebac82a9ec31bee
asanford99 <98435142+asanford99@users.noreply.github.com>
2022-03-27 00:52:16 -0700

    Updated and working fops.c file
    Working file with the load_file and save_file functions for reading and writing into a .csv file.

--- address_book_fops.c
+++ address_book_fops.c
@@ -18,9 +18,12 @@ Status load_file(AddressBook *address_book)
 	int phoneNumbers = 0;
 	int emailAddresses = 0;
 	int i;
+	int j;
 
 	char c;
 	char fileInput[32] = "";
+
+	address_book->list = (ContactInfo*)malloc(sizeof(ContactInfo));
 
 	address_book->fp = fopen(DEFAULT_FILE, "r");
 
@@ -41,11 +44,26 @@ Status load_file(AddressBook *address_book)
 	}
 	else{
 
+		c = getc(address_book->fp);
+
+		if (c == EOF){
+
+			goto EntrySet;
+
+		}
+
 		for (c = getc(address_book->fp); c != EOF; c = getc(address_book->fp)){
 
         	if (c == '\n'){
 
-            	entries++;
+				c = getc(address_book->fp);
+
+				if (c != ','){
+
+					entries++;
+
+				}
+            	
 
 			}
 		
@@ -53,9 +71,34 @@ Status load_file(AddressBook *address_book)
 
 		fclose(address_book->fp);
 
+		entries++;
+
+		EntrySet:
+
+		address_book->count = (entries);
+
 		address_book->fp = fopen(DEFAULT_FILE, "r");
 
 		address_book->list = malloc((entries + 1) * sizeof(ContactInfo));
+
+		for (i = 0; i < entries; i++){
+
+			strcpy(address_book->list[i].name[0], "");
+
+			for (j = 0; j < 5; j++){
+
+				strcpy(address_book->list[i].phone_numbers[j], "");
+
+			}
+
+			for (j = 0; j < 5; j++){
+
+				strcpy(address_book->list[i].email_addresses[j], "");
+
+			}
+
+
+		}
 
 		for (c = getc(address_book->fp); c != EOF; c = getc(address_book->fp)){
 
@@ -105,7 +148,7 @@ Status load_file(AddressBook *address_book)
 								if (phoneNumbers <= 5){
 
 									strcpy(address_book->list[currentEntry - 1].phone_numbers[phoneNumbers - 1], fileInput);	
-
+									
 								}
 
 								strcpy(fileInput, "");
@@ -134,7 +177,7 @@ Status load_file(AddressBook *address_book)
 								if (emailAddresses <= 5){
 
 									strcpy(address_book->list[currentEntry - 1].email_addresses[emailAddresses - 1], fileInput);	
-
+									
 								}
 
 								strcpy(fileInput, "");
@@ -157,19 +200,73 @@ Status load_file(AddressBook *address_book)
         
 	}
 
-	fclose(address_book->fp);
+	if (fileInput[0] != '\0'){
 
-	printf("Loaded address book successfully.\n");
+		emailAddresses++;
+
+		if (emailAddresses <= 5){
+
+			strcpy(address_book->list[currentEntry - 1].email_addresses[emailAddresses - 1], fileInput);	
+	
+		}
+
+		strcpy(fileInput, "");
+
+	}
+
+	fclose(address_book->fp);
+	printf("Successfully loaded file.\n");
 
 	return e_success;
 }
 
 Status save_file(AddressBook *address_book)
 {
-	/*
-	 * Write contacts back to file.
-	 * Re write the complete file currently
-	 */ 
+
+	int *phoneNumbers;
+	int *emailAddresses;
+	int entries = 0;
+	int phoneNumbersTraversed = 0;
+	int emailAddressesTraversed = 0;
+	int maxDataCount = 0;
+	int i;
+	int j;
+
+	entries = address_book->count;
+
+	phoneNumbers = malloc((entries + 1) * sizeof(int));
+	emailAddresses = malloc((entries + 1) * sizeof(int));
+
+	for (i = 0; i < entries; i++){
+
+		phoneNumbers[i] = 0;
+
+	}
+
+	for (i = 0; i < entries; i++){
+
+		emailAddresses[i] = 0;
+
+	}
+
+
+
+	for (i = 0; i < entries; i++){
+
+		for (j = 0; address_book->list[i].phone_numbers[j][0] != '\0'; j++){
+
+			phoneNumbers[i] += 1;
+
+		}
+
+		for (j = 0; address_book->list[i].email_addresses[j][0] != '\0'; j++){
+
+			emailAddresses[i] += 1;
+
+		}
+
+	}
+
 	address_book->fp = fopen(DEFAULT_FILE, "w");
 
 	if (address_book->fp == NULL)
@@ -177,12 +274,71 @@ Status save_file(AddressBook *address_book)
 		return e_fail;
 	}
 
-	/* 
-	 * Add the logic to save the file
-	 * Make sure to do error handling
-	 */ 
+	for (i = 0; i < entries; i++){
+
+		if (phoneNumbers[i] >= emailAddresses[i]){
+
+			maxDataCount = phoneNumbers[i];
+
+		}
+		else{
+
+			maxDataCount = emailAddresses[i];
+
+		}
+
+		if (maxDataCount < 1){
+
+			maxDataCount = 1;
+
+		}
+
+		for (j = 0; j < maxDataCount; j++){
+
+			if (j == 0){
+
+				fputs(address_book->list[i].name[0], address_book->fp);
+
+			}
+
+			fputc(',', address_book->fp);
+
+			if(phoneNumbers[i] > 0){
+
+				fputs(address_book->list[i].phone_numbers[j], address_book->fp);
+
+			}
+
+			fputc(',', address_book->fp);
+
+			if(emailAddresses[i] > 0){
+
+				fputs(address_book->list[i].email_addresses[j], address_book->fp);
+
+			}
+
+			if (i != (entries - 1)){
+
+				fputc('\n', address_book->fp);
+
+			}
+
+			phoneNumbers[i]--;
+			emailAddresses[i]--;
+
+		}
+
+
+		
+
+
+	}
+
 
 	fclose(address_book->fp);
+
+	free(phoneNumbers);
+	free(emailAddresses);
 
 	return e_success;
 }
